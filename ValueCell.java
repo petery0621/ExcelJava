703260710018b1bfecafc24837a5e77cd02ccb02
petery0621 <petery0621@gmail.com>
2015-03-03 17:23:17 -0800

    Create ValueCell.java

---
+++ ValueCell.java
@@ -0,0 +1,43 @@
+package Excel;
+
+public class ValueCell extends Cell
+{
+
+//	private String value = "";
+	
+	public ValueCell(){
+	}
+	
+	public ValueCell(String newValue) {
+//		super(newValue);
+		this.value = newValue;
+	}
+
+	public String fullCellText()
+	{
+		return value;
+	}
+	
+	public String abbreviatedCellText()
+	{
+		value = formatInput(value);
+		return value;
+	}
+	
+	public String getValue()
+	{
+		value = formatInput(value);
+		return value;
+	}
+
+	public void setValue(String newValue)
+	{
+		this.value = newValue;
+	}
+	
+	public void specFormat(){
+		
+	}
+
+	
+}
