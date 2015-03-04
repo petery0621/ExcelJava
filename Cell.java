924bb3651d279593b836cd9ce15b3ee54b860337
petery0621 <petery0621@gmail.com>
2015-03-03 23:09:31 -0800

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -5,11 +5,13 @@
 package Excel;
 
 abstract class Cell {
-
+	// This is the parent class for 
+	// EmptyCell, ValueCell, TextCell, DateCell
+	
 	public String value = "";
 	
 	public Cell(){
-		
+		// Empty no-argument constructor
 	}
 
 	public Cell(String newValue)
@@ -27,6 +29,7 @@ abstract class Cell {
 		this.value = newValue;
 	}
 	
+	// Not in use
 	public String fullCellText()
 	{
 		String empty = "";
@@ -35,6 +38,7 @@ abstract class Cell {
 		}
 		return empty;
 	}
+
 	public String abbreviatedCellText()
 	{
 		String empty = "";
@@ -45,33 +49,47 @@ abstract class Cell {
 	}
 	
 	// To truncate the longer strings and "centered" them
-	// Called when displaying the whole  
-	public String formatInput(String newInput)
-	{ 
+	// Called when displaying the whole
+	// To do: for negative real numbers, you may want to align 
+	// them according to the left first digits
+	public String formatInput(String newInput){ 
+		// To gaurantee there will not be errors
+		// when calling "newInput.charAt(0)" at Line 69
+		
+		if (newInput.isEmpty()){
+			System.out.println("formatInput: newInput is empty");
+			return "";
+		}
+		
 		Spreadsheet sheet = new Spreadsheet();
 		String out = "";
-		for(int i = 0; i < (sheet.COL_WIDTH - newInput.length())/2; i++)
-		{
+		int limit = (sheet.COL_WIDTH - newInput.length())/2
+		
+		if(newInput.length()>=2 && 
+		   newInput.charAt(0).equals('-') &&
+		   Character.isDigit(newInput.charAt(1)))
+			limit --; 
+		
+		for(int i = 0; i < limit; i++){
 			out = out + " ";
 		}
-		if (newInput.length() > 12)
-		{
+		
+		if (newInput.length() >= 12){
 			out = newInput.substring(0, 11) + ">";
-		}
-		else
-		{
+		}else{
 			out = out + newInput;
 		}
+		
 		int numSpaces = sheet.COL_WIDTH - out.length();
-		for(int j = 0; j < numSpaces; j++)
-		{
+		for(int j = 0; j < numSpaces; j++){
 			out = out + " ";
 		}
 		return out;
 	}
 	
 	public String specFormat(){
-		// for displaying the whole sheet
+		// This function is on purpose left blank, 
+		// in order for subclasses to override it
 		return "Warning: msg not overriden"; 
 	}
 		
